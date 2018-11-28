4e21e07bdafade955e201f9984b621949f002b74
John <stableford.john@gmail.com>
2018-11-28 16:42:59 +1100

    Update slack-bulkinviter.py

--- slack-bulkinviter.py
+++ slack-bulkinviter.py
@@ -48,14 +48,14 @@ for user_id, user_name, is_bot, is_deleted in users:
     num += 1
     print("{}. Inviting {} to {}".format(num, user_name, channel_name))
 
-    # try:
-    #     slack.channels.invite(channel_id, user_id)
-    # except Error as e:
-    #     code = e.args[0]
-    #     if code == "already_in_channel":
-    #         print("{} is already in the channel".format(user_name))
-    #     elif code in ('cant_invite_self', 'cant_invite', 'user_is_ultra_restricted'):
-    #         print("Skipping user {} ('{}')".format(user_name, code))
-    #     else:
-    #         raise
+    try:
+        slack.channels.invite(channel_id, user_id)
+    except Error as e:
+        code = e.args[0]
+        if code == "already_in_channel":
+            print("{} is already in the channel".format(user_name))
+        elif code in ('cant_invite_self', 'cant_invite', 'user_is_ultra_restricted'):
+            print("Skipping user {} ('{}')".format(user_name, code))
+        else:
+            raise
 
